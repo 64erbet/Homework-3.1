030960fc6acd4a7b580615ebc5ef4a3240a1d284
64erbet <fox2703@gmail.com>
2022-08-31 18:40:22 +0300

    Added files

---
+++ initDB.sql
@@ -0,0 +1,154 @@
+﻿create schema if not exists public;
+
+create database Task_3 with owner postgres;
+
+comment on schema public is 'standard public schema';
+
+alter schema public owner to postgres;
+
+CREATE TABLE companies 
+(
+	companyId SERIAL NOT NULL,
+	
+	companyName VARCHAR,
+	companyLocation VARCHAR
+);
+
+alter table companies owner to postgres;
+
+CREATE TABLE developers 
+(
+	developerId SERIAL NOT NULL,
+	firstName VARCHAR,
+	gender VARCHAR,
+	age INT,
+	reportDay DATE,
+	companyId INT
+);
+
+alter table developers owner to postgres;
+
+CREATE TABLE customers
+(
+	customerId SERIAL NOT NULL,
+	
+	customerName VARCHAR(50),
+	customerLocation VARCHAR
+);
+
+ALTER TABLE customers owner TO postgres;
+
+CREATE TABLE projects
+(
+	projectId SERIAL NOT NULL,
+	
+	projectName VARCHAR(50),
+	customerId INT,
+	workersAmount INT
+);
+
+alter table projects owner to postgres;
+
+
+CREATE TABLE skills
+(
+	skillId SERIAL NOT NULL,
+	
+	skillName VARCHAR(50),
+	skillLavel VARCHAR(10)
+);
+
+alter table skills owner to postgres;
+
+
+-- *************************************************************************
+
+ALTER TABLE customers ADD PRIMARY KEY(customerId);
+ALTER TABLE projects ADD PRIMARY KEY(projectId);
+ALTER TABLE companies ADD PRIMARY KEY(companyId);
+ALTER TABLE skills ADD PRIMARY KEY(skillId);
+ALTER TABLE developers ADD PRIMARY KEY(developerId);
+
+-- *************************************************************************
+
+CREATE TABLE developers_projects
+(
+	dp_developer_id INT,
+	dp_project_id INT
+);
+
+ALTER TABLE developers_projects owner TO postgres;
+
+CREATE TABLE developers_skills
+(
+	ds_developer_id INT,
+	ds_skill_id INT
+);
+
+ALTER TABLE developers_skills owner TO postgres;
+
+CREATE TABLE companyes_projects
+(
+	cmppr_company_id INT,
+	cmppr_project_id INT
+);
+
+ALTER TABLE companyes_projects owner TO postgres;
+
+CREATE TABLE customers_projects
+(
+	cuspr_customer_id INT,
+	cuspr_project_id INT
+);
+
+ALTER TABLE customers_projects owner TO postgres;
+
+
+-- *************************************************************************
+
+ALTER TABLE developers ADD CONSTRAINT dev_fk FOREIGN KEY (coppanyid)
+											 REFERENCES companies(companyid);
+											 
+ALTER TABLE projects ADD CONSTRAINT proj_fk FOREIGN KEY (customerid)
+											 REFERENCES customers(customerid);
+
+-- *************************************************************************
+
+ALTER TABLE developers_projects 
+	ADD CONSTRAINT dp_dev_id 
+	FOREIGN KEY (dp_developer_id)
+	REFERENCES developers(developerid);
+ALTER TABLE developers_projects 
+	ADD CONSTRAINT dp_poj_id 
+	FOREIGN KEY (dp_project_id)											 
+	REFERENCES projects(projectid);
+
+ALTER TABLE developers_skills 
+	ADD CONSTRAINT ds_dev_id 
+	FOREIGN KEY (ds_developer_id)
+	REFERENCES developers(developerid);
+ALTER TABLE developers_skills 
+	ADD CONSTRAINT ds_skill_id
+	FOREIGN KEY (ds_skill_id)											 
+	REFERENCES skills(skillid);
+
+ALTER TABLE companyes_projects
+	ADD CONSTRAINT cp_com_id 
+	FOREIGN KEY (cmppr_company_id)
+	REFERENCES companies(companyid);
+ALTER TABLE companyes_projects
+	ADD CONSTRAINT cp_project_id
+	FOREIGN KEY (cmppr_project_id)											 
+	REFERENCES projects(projectid);
+
+ALTER TABLE customers_projects
+	ADD CONSTRAINT cusp_cus_id 
+	FOREIGN KEY (cuspr_customer_id)
+	REFERENCES customers(customerid);
+ALTER TABLE customers_projects
+	ADD CONSTRAINT cp_project_id
+	FOREIGN KEY (cuspr_project_id)											 
+	REFERENCES projects(projectid);
+	
+--*****************************************************************
+
---
+++ populateDB.sql
@@ -0,0 +1,64 @@
+﻿INSERT INTO public.companies (companyid, companyname, companylocation) VALUES (1, 'Adidas', 'Ukraine, Kyiv');
+INSERT INTO public.companies (companyid, companyname, companylocation) VALUES (2, 'Nike', 'Ukraine, Kharkiv');
+INSERT INTO public.companies (companyid, companyname, companylocation) VALUES (3, 'Puma', 'Ukraine, Dnipro');
+INSERT INTO public.companies (companyid, companyname, companylocation) VALUES (4, 'Everlast', 'USA, New York');
+INSERT INTO public.companies (companyid, companyname, companylocation) VALUES (5, 'Reyvel', 'Ukraine, Ivano-Frankivsk');
+
+INSERT INTO public.developers (developerid, firstname, gender, age, reportday, companyid) VALUES (1, 'Alex', 'Male', 35, to_date('2022-Dec-25', 'YYYY-Mon-DD'), 5);
+INSERT INTO public.developers (developerid, firstname, gender, age, reportday, companyid) VALUES (2, 'Serhiy', 'Male', 33, to_date('2021-Feb-25', 'YYYY-Mon-DD'), 4);
+INSERT INTO public.developers (developerid, firstname, gender, age, reportday, companyid) VALUES (3, 'Andriy', 'Male', 90, to_date('2022-Jan-25', 'YYYY-Mon-DD'), 3);
+INSERT INTO public.developers (developerid, firstname, gender, age, reportday, companyid) VALUES (4, 'Rostislav', 'Male', 65, to_date('2022-Mar-25', 'YYYY-Mon-DD'), 2);
+INSERT INTO public.developers (developerid, firstname, gender, age, reportday, companyid) VALUES (5, 'Olena', 'Female', 10, to_date('2002-Dec-01', 'YYYY-Mon-DD'), 1);
+
+INSERT INTO public.customers (customerid, customername, customerlocation) VALUES (1, 'Alfred', 'Germany, Hamburg');
+INSERT INTO public.customers (customerid, customername, customerlocation) VALUES (2, 'School_#15', 'Germany, Hamburg');
+INSERT INTO public.customers (customerid, customername, customerlocation) VALUES (3, 'Oksford', 'England');
+INSERT INTO public.customers (customerid, customername, customerlocation) VALUES (4, 'Cambridge', 'England');
+INSERT INTO public.customers (customerid, customername, customerlocation) VALUES (5, 'KPI', 'Ukraine, Kyiv');
+
+INSERT INTO public.projects (projectid, projectname, customerid, workersamount) VALUES (1, 'Project_1', 5, 10000);
+INSERT INTO public.projects (projectid, projectname, customerid, workersamount) VALUES (2, 'Project_2', 4, 100);
+INSERT INTO public.projects (projectid, projectname, customerid, workersamount) VALUES (3, 'Project_3', 3, 10);
+INSERT INTO public.projects (projectid, projectname, customerid, workersamount) VALUES (4, 'Project_4', 2, 30);
+INSERT INTO public.projects (projectid, projectname, customerid, workersamount) VALUES (5, 'Project_5', 1, 50);
+
+INSERT INTO public.skills (skillid, skillname, skilllavel) VALUES (1, 'JavaScript', 'Junior');
+INSERT INTO public.skills (skillid, skillname, skilllavel) VALUES (2, 'Pyton', 'Middle');
+INSERT INTO public.skills (skillid, skillname, skilllavel) VALUES (3, 'Java', 'Senior');
+INSERT INTO public.skills (skillid, skillname, skilllavel) VALUES (4, 'html', 'Middle');
+INSERT INTO public.skills (skillid, skillname, skilllavel) VALUES (5, 'Fortran', 'Senior');
+
+--**************************************************************************************
+
+INSERT INTO public.developers_projects (dp_developer_id, dp_project_id) VALUES (1, 2);
+INSERT INTO public.developers_projects (dp_developer_id, dp_project_id) VALUES (1, 3);
+INSERT INTO public.developers_projects (dp_developer_id, dp_project_id) VALUES (1, 5);
+INSERT INTO public.developers_projects (dp_developer_id, dp_project_id) VALUES (3, 1);
+INSERT INTO public.developers_projects (dp_developer_id, dp_project_id) VALUES (4, 2);
+INSERT INTO public.developers_projects (dp_developer_id, dp_project_id) VALUES (5, 1);
+
+INSERT INTO public.developers_skills (ds_developer_id, ds_skill_id) VALUES (1, 1);
+INSERT INTO public.developers_skills (ds_developer_id, ds_skill_id) VALUES (1, 2);
+INSERT INTO public.developers_skills (ds_developer_id, ds_skill_id) VALUES (1, 3);
+INSERT INTO public.developers_skills (ds_developer_id, ds_skill_id) VALUES (1, 4);
+INSERT INTO public.developers_skills (ds_developer_id, ds_skill_id) VALUES (2, 2);
+INSERT INTO public.developers_skills (ds_developer_id, ds_skill_id) VALUES (2, 4);
+INSERT INTO public.developers_skills (ds_developer_id, ds_skill_id) VALUES (3, 1);
+INSERT INTO public.developers_skills (ds_developer_id, ds_skill_id) VALUES (4, 4);
+INSERT INTO public.developers_skills (ds_developer_id, ds_skill_id) VALUES (4, 5);
+INSERT INTO public.developers_skills (ds_developer_id, ds_skill_id) VALUES (5, 1);
+INSERT INTO public.developers_skills (ds_developer_id, ds_skill_id) VALUES (5, 2);
+
+INSERT INTO public.companyes_projects (cmppr_company_id, cmppr_project_id) VALUES (1, 2);
+INSERT INTO public.companyes_projects (cmppr_company_id, cmppr_project_id) VALUES (1, 4);
+INSERT INTO public.companyes_projects (cmppr_company_id, cmppr_project_id) VALUES (1, 5);
+INSERT INTO public.companyes_projects (cmppr_company_id, cmppr_project_id) VALUES (2, 1);
+INSERT INTO public.companyes_projects (cmppr_company_id, cmppr_project_id) VALUES (3, 2);
+INSERT INTO public.companyes_projects (cmppr_company_id, cmppr_project_id) VALUES (4, 5);
+INSERT INTO public.companyes_projects (cmppr_company_id, cmppr_project_id) VALUES (5, 5);
+
+INSERT INTO public.customers_projects (cuspr_customer_id, cuspr_project_id) VALUES (1, 2);
+INSERT INTO public.customers_projects (cuspr_customer_id, cuspr_project_id) VALUES (1, 3);
+INSERT INTO public.customers_projects (cuspr_customer_id, cuspr_project_id) VALUES (1, 5);
+INSERT INTO public.customers_projects (cuspr_customer_id, cuspr_project_id) VALUES (2, 1);
+INSERT INTO public.customers_projects (cuspr_customer_id, cuspr_project_id) VALUES (3, 4);
